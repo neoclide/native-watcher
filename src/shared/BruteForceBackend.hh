4f11e0ec8b838d1e22b0b12041a87d4a38a71198
Qiming zhao <chemzqm@gmail.com>
2026-09-24 15:28:33 +0800

    fix: throw std::runtime_error instead of string literal in BruteForceBackend

--- src/shared/BruteForceBackend.hh
+++ src/shared/BruteForceBackend.hh
@@ -1,6 +1,7 @@
 #ifndef BRUTE_FORCE_H
 #define BRUTE_FORCE_H
 
+#include <stdexcept>
 #include "../Backend.hh"
 #include "../DirTree.hh"
 #include "../Watcher.hh"
@@ -8,11 +9,11 @@
 class BruteForceBackend : public Backend {
 public:
   void subscribe(WatcherRef watcher) override {
-    throw "Brute force backend doesn't support subscriptions.";
+    throw std::runtime_error("Brute force backend doesn't support subscriptions.");
   }
 
   void unsubscribe(WatcherRef watcher) override {
-    throw "Brute force backend doesn't support subscriptions.";
+    throw std::runtime_error("Brute force backend doesn't support subscriptions.");
   }
 
   std::shared_ptr<DirTree> getTree(WatcherRef watcher, bool shouldRead = true);
